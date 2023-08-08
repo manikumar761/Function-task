2b4ae2e82fee05d251a36d08d0054bb7bc036f2f
manikumar761 <manitechstar2023@gmail.com>
2023-08-08 06:36:47 +0530

    Day 3 task

---
+++ script.js
@@ -0,0 +1,18 @@
+//function method
+
+// var a = [1,2,3,4,5,6,7,8,9]
+// var result = []
+// function odd (a)
+// {
+//     for(var i = 0;i < a.length; i++)
+//     {
+//         if(a[i]%2!=0)
+//         {
+//            result.push(a[i])
+//         }
+//     } return result
+// }
+// console.log (odd(a))
+
+// anonymous function
+//
